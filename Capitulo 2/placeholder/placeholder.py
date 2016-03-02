0176bb73c4749e1464a7454ec099cacedc32a2bf
Eduardo Henrique Kenji Shibukawa <EduardoShibuka@gmail.com>
2016-03-02 15:59:06 -0300

    Adicionado cache
    Adicionado cache para armazernar a figura

--- Capitulo 2/placeholder/placeholder.py
+++ Capitulo 2/placeholder/placeholder.py
@@ -27,6 +27,7 @@ from django.core.wsgi import get_wsgi_application
 from django.http import HttpResponse, HttpResponseBadRequest
 from io import BytesIO
 from PIL import Image, ImageDraw
+from django.core.cache import cache
 
 class ImageForm(forms.Form):
     """Formulário para validar o placeholder de imagem solicitado"""
@@ -37,17 +38,21 @@ class ImageForm(forms.Form):
         """Gera uma imagem do tipo especificado e a retorna na forma de bytes puros."""
         height = self.cleaned_data['height']
         width = self.cleaned_data['width']
-        image = Image.new('RGB', (width, height))
-        draw = ImageDraw.Draw(image)
-        text = '{} x {}'.format(width, height)
-        textwidth, textheight = draw.textsize(text)
-        if textwidth < width and textheight < height:
-            texttop = (height - textheight) // 2
-            textleft = (width - textwidth) // 2
-            draw.text((textleft, texttop), text, fill=(0,255,255))
-        content = BytesIO()
-        image.save(content, image_format)
-        content.seek(0)
+        key='{}.{}.{}'.format(width, height, image_format)
+        content = cache.get(key)
+        if content is None:
+            image = Image.new('RGB', (width, height))
+            draw = ImageDraw.Draw(image)
+            text = '{} x {}'.format(width, height)
+            textwidth, textheight = draw.textsize(text)
+            if textwidth < width and textheight < height:
+                texttop = (height - textheight) // 2
+                textleft = (width - textwidth) // 2
+                draw.text((textleft, texttop), text, fill=(0,255,255))
+            content = BytesIO()
+            image.save(content, image_format)
+            content.seek(0)
+            cache.set(key, content, 60*60)
         return content
 
 
